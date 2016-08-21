6554f81f3f76bc9d95350dc9c27a3185390afd95
Ankai Jie <ankaijie@gmail.com>
2016-08-21 18:56:52 -0400

    fixed logout, input validation, and minor style changes

--- controllers/tracking-app.js
+++ controllers/tracking-app.js
@@ -1,201 +1,234 @@
-var app = angular.module('tracking-app', [ 'ngRoute', 'ui.bootstrap' ]);
+var app = angular.module('tracking-app', ['ngRoute', 'ui.bootstrap']);
 
-app.config([ '$routeProvider', function($routeProvider) {
-	$routeProvider.when('/login', {
-		templateUrl : 'views/login.html',
-		controller : 'LoginController'
-	});
-	$routeProvider.when('/homeTracker', {
-		templateUrl : 'views/trackerHome.html',
-		controller : 'TrackerHomeController'
-	});
-	$routeProvider.when('/homeSubject', {
-		templateUrl : 'views/subjectHome.html',
-		controller : 'SubjectHomeController'
-	});
-	$routeProvider.otherwise({
-		redirectTo : '/login'
-	});
-} ]);
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginController'
+    });
+    $routeProvider.when('/homeTracker', {
+        templateUrl: 'views/trackerHome.html',
+        controller: 'TrackerHomeController'
+    });
+    $routeProvider.when('/homeSubject', {
+        templateUrl: 'views/subjectHome.html',
+        controller: 'SubjectHomeController'
+    });
+    $routeProvider.otherwise({
+        redirectTo: '/login'
+    });
+}]);
 
 app.service('userDataTransfer', function() {
-	var data = {};
+    var data = {};
 
-	return {
-		getData : function() {
-			return data;
-		},
+    return {
+        getData: function() {
+            return data;
+        },
 
-		setData : function(info) {
-			data = info;
-		}
-	}
+        setData: function(info) {
+            data = info;
+        }
+    }
 });
+
 
 app.factory('subjectsFactory', function($http, $log) {
 
-	var factory = {};
-	factory.getSubjects = function(trackerName) {
-		return $http.get('/getTrackerSubjects?trackerName=' + trackerName).then(function(response) {
-			$log.log(response);
-			return response.data;
-		});
-	}
-	return factory;
+    var factory = {};
+    factory.getSubjects = function(trackerName) {
+        return $http.get('/getTrackerSubjects?trackerName=' + trackerName).then(function(response) {
+            $log.log(response);
+            return response.data;
+        });
+    }
+    return factory;
+});
+
+app.controller('HeaderController', function($scope, $log, $location, $route) {
+
+    $scope.loggedIn = false;
+
+    $scope.reset = function() {
+        $location.url('/');
+    }
 });
 
 app.controller('LoginController', function($scope, $location, $modal, $log, $http, userDataTransfer) {
-	// $scope.load = function(path){
-	// $log.log("Redirect to: " + path);
-	// $location.url(path);
-	// }
-	$scope.userTypes = [ 'Tracker', 'Subject' ];
+    // $scope.load = function(path){
+    // $log.log("Redirect to: " + path);
+    // $location.url(path);
+    // }
+    $scope.userTypes = ['Tracker', 'Subject'];
 
-	$scope.userTypeSelected = 'Select User Type';
+    $scope.noUserType = false;
+    $scope.badLogin = false;
+    $scope.userTypeSelected = 'Select User Type';
 
-	$scope.login = function() {
-		var user_data = $('#user-login').serialize();
-		var url = 'login' + $scope.userTypeSelected;
-		$scope.post(user_data, url);
-	}
+    $scope.login = function() {
 
-	$scope.post = function(data, url) {
-		$log.log("Entered Post Function");
-		$http({
-			url : url,
-			method : 'post',
-			headers : {
-				'Content-Type' : 'application/x-www-form-urlencoded'
-			},
-			data : data
-		}).success(function(data, status, headers, config) {
-			$log.log('Login Success. Data: ' + data);
-			userDataTransfer.setData(data);
-			$location.url('/home' + $scope.userTypeSelected);
-		}).error(function(data, status, headers, config) {
-			$log.log('Login Unsuccessful. Error: ' + error);
-		})
-	}
+        if ($scope.userTypeSelected === 'Select User Type') {
+            $scope.noUserType = true;
+        } else {
+            $scope.noUserType = false;
+            var user_data = $('#user-login').serialize();
+            var url = 'login' + $scope.userTypeSelected;
+            $scope.post(user_data, url);
+        }
 
-	$scope.register = function() {
-		$scope.openModal();
-	}
+    }
 
-	$scope.openModal = function() {
-		var modalInstance = $modal.open({
-			// scope : $scope,
-			templateUrl : 'views/userReg.html',
-			controller : 'UserRegController'
-		});
-	}
+    $scope.post = function(data, url) {
+        $log.log("Entered Post Function");
+        $http({
+            url: url,
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: data
+        }).success(function(data, status, headers, config) {
+            $scope.badLogin = false;
+            $log.log('Login Success. Data: ' + data);
+            userDataTransfer.setData(data);
+            $location.url('/home' + $scope.userTypeSelected);
+        }).error(function(data, status, headers, config) {
+            $log.log('Login Unsuccessful. Error: ' + data);
+            if (status === 400 || status === 401) {
+                $scope.badLogin = true;
+            }
+        })
+    }
+
+    $scope.register = function() {
+        $scope.openModal();
+    }
+
+    $scope.openModal = function() {
+        var modalInstance = $modal.open({
+            // scope : $scope,
+            templateUrl: 'views/userReg.html',
+            controller: 'UserRegController'
+        });
+    }
 });
 
 app.controller('UserRegController', function($scope, $log, $modalInstance, $http) {
 
-	$scope.userTypes = [ 'Tracker', 'Subject' ];
+    $scope.userTypes = ['Tracker', 'Subject'];
 
-	$scope.userTypeSelected = $scope.userTypes[0];
+    $scope.userTypeSelected = $scope.userTypes[0];
 
-	$scope.modalOk = function() {
-		$log.log('Form Submitted')
-		$scope.post();
-		$modalInstance.close('ok');
-	};
+    $scope.modalOk = function() {
+        $log.log('Form Submitted')
+        $scope.post();
+        $modalInstance.close('ok');
+    };
 
-	$scope.modalCancel = function() {
-		$log.log('User Pressed Cancel')
-		$modalInstance.dismiss('cancel');
-	};
+    $scope.modalCancel = function() {
+        $log.log('User Pressed Cancel')
+        $modalInstance.dismiss('cancel');
+    };
 
-	$scope.post = function() {
-		$log.log("Entered Register Post Function");
-		$http({
-			url : '/addNew' + $scope.userTypeSelected,
-			method : 'post',
-			headers : {
-				'Content-Type' : 'application/x-www-form-urlencoded'
-			},
-			data : $('#add-new-user').serialize()
-		}).success(function(data, status, headers, config) {
-			$log.log('Register Success. Data: ' + data);
-		}).error(function(data, status, headers, config) {
-			$log.log('Register Unsuccessful. Error: ' + error);
-		})
-	}
-
-});
-
-app.controller('TrackerHomeController', function($scope, $log, $modal, $http, userDataTransfer, subjectsFactory) {
-	$scope.tracker = userDataTransfer.getData();
-
-	subjectsFactory.getSubjects($scope.tracker.username).then(function(data) {
-		$scope.subjects = data;
-	});
-
-	$scope.addSubjectDialog = function() {
-		var modalInstance = $modal.open({
-			scope : $scope,
-			templateUrl : 'views/addSubjectDialog.html',
-			controller : function($scope, $modalInstance, $log) {
-
-				$scope.newSubjectId;
-
-				$scope.ok = function() {
-					$log.log('form submitted.')
-					$scope.getNewTracker($scope.newSubjectId);
-					$modalInstance.close('ok');
-				};
-
-				$scope.pressedCancel = function() {
-					$log.log('closing.')
-					$modalInstance.dismiss('cancel');
-				};
-			}
-		});
-	}
-	$scope.getNewTracker = function(subjectId) {
-		$log.log("Entered getNewTracker Function");
-		$http.get('/addSubjectToTracker?trackerName=' + $scope.tracker.username + '&subjectTrackingId=' + subjectId).then(function(response) {
-			$log.log(response);
-			$scope.tracker = response.data;
-			$scope.refreshSubjects();
-		});
-	}
-	
-	$scope.deleteSubject = function (subjectId){
-		$log.log("Entered deleteSubject Function");
-		$http.get('/deleteSubjectFromTracker?trackerName=' + $scope.tracker.username + '&subjectTrackingId=' + subjectId).then(function(response) {
-			$log.log(response);
-			$scope.tracker = response.data;
-			$scope.refreshSubjects();
-		});
-	}
-
-	$scope.refreshSubjects = function() {
-		subjectsFactory.getSubjects($scope.tracker.username).then(function(data) {
-			$scope.subjects = data;
-		});
-	}
+    $scope.post = function() {
+        $log.log("Entered Register Post Function");
+        $http({
+            url: '/addNew' + $scope.userTypeSelected,
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: $('#add-new-user').serialize()
+        }).success(function(data, status, headers, config) {
+            $log.log('Register Success. Data: ' + data);
+        }).error(function(data, status, headers, config) {
+            $log.log('Register Unsuccessful. Error: ' + error);
+        })
+    }
 
 });
 
-app.controller('SubjectHomeController', function($scope, $log, $http, userDataTransfer) {
-	$scope.subject = userDataTransfer.getData();
+app.controller('TrackerHomeController', function($scope, $log, $modal, $http, $location, $window, userDataTransfer, subjectsFactory) {
+    $scope.tracker = userDataTransfer.getData();
 
-	$scope.updateLocation = function() {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			$log.log(position);
-			$scope.lon = position.coords.longitude;
-			$scope.lat = position.coords.latitude;
-			$scope.updateSubject();
-		});
-	}
+    subjectsFactory.getSubjects($scope.tracker.username).then(function(data) {
+        $scope.subjects = data;
+    });
 
-	$scope.updateSubject = function() {
-		$http.get('/subjectLocation?subUsername=' + $scope.subject.username + '&longitude=' + $scope.lon + '&latitude=' + $scope.lat).then(function(response) {
-			$log.log('update Subject data: ' + response.data);
-			$scope.subject = response.data;
-		});
-	}
+    $scope.addSubjectDialog = function() {
+        var modalInstance = $modal.open({
+            scope: $scope,
+            templateUrl: 'views/addSubjectDialog.html',
+            controller: function($scope, $modalInstance, $log) {
+
+                $scope.newSubjectId;
+
+                $scope.ok = function() {
+                    $log.log('form submitted.')
+                    $scope.getNewTracker($scope.newSubjectId);
+                    $modalInstance.close('ok');
+                };
+
+                $scope.pressedCancel = function() {
+                    $log.log('closing.')
+                    $modalInstance.dismiss('cancel');
+                };
+            }
+        });
+    }
+    $scope.getNewTracker = function(subjectId) {
+        $log.log("Entered getNewTracker Function");
+        $http.get('/addSubjectToTracker?trackerName=' + $scope.tracker.username + '&subjectTrackingId=' + subjectId).then(function(response) {
+            $log.log(response);
+            $scope.tracker = response.data;
+            $scope.refreshSubjects();
+        });
+    }
+
+    $scope.deleteSubject = function(subjectId) {
+        $log.log("Entered deleteSubject Function");
+        $http.get('/deleteSubjectFromTracker?trackerName=' + $scope.tracker.username + '&subjectTrackingId=' + subjectId).then(function(response) {
+            $log.log(response);
+            $scope.tracker = response.data;
+            $scope.refreshSubjects();
+        });
+    }
+
+    $scope.refreshSubjects = function() {
+        subjectsFactory.getSubjects($scope.tracker.username).then(function(data) {
+            $scope.subjects = data;
+        });
+    }
+
+    $scope.logout = function() {
+        $http.get('/logout');
+        $location.url('/');
+    }
+
+});
+
+app.controller('SubjectHomeController', function($scope, $log, $location, $http, userDataTransfer) {
+    $scope.subject = userDataTransfer.getData();
+
+    $scope.updateLocation = function() {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            $log.log(position);
+            $scope.lon = position.coords.longitude;
+            $scope.lat = position.coords.latitude;
+            $scope.updateSubject();
+        });
+    }
+
+    $scope.updateSubject = function() {
+        $http.get('/subjectLocation?subUsername=' + $scope.subject.username + '&longitude=' + $scope.lon + '&latitude=' + $scope.lat).then(function(response) {
+            $log.log('update Subject data: ' + response.data);
+            $scope.subject = response.data;
+        });
+    }
+
+    $scope.logout = function() {
+        $http.get('/logout');
+        $location.url('/');
+    }
 
 });
--- server.js
+++ server.js
@@ -10,11 +10,13 @@ var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var request = require('request');
+var cookieParser = require('cookie-parser');
+
 
 // General config
 mongoose.connect(database.url);
 app.use(bodyParser.urlencoded({
-	'extended' : 'true'
+    'extended': 'true'
 })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());
 // app.use(require('express-session')({
@@ -23,12 +25,14 @@ app.use(bodyParser.json());
 // saveUninitialized : false
 // }));
 app.use(session({
-	secret : 'Ankai Secret Session',
-	resave : false,
-	saveUninitialized : false
+    secret: 'Ankai Secret Session',
+    resave: false,
+    saveUninitialized: false
 }));
+
+app.use(cookieParser());
 app.use(passport.initialize());
-app.use(passport.session());// passport is for user authentication
+app.use(passport.session()); // passport is for user authentication
 
 // Setting up router
 var router = express.Router();
@@ -100,50 +104,52 @@ passport.deserializeUser(Tracker.deserializeUser());
 // set up server for html pages
 app.use(express.static(path.resolve("./")));
 app.get('/', function(req, res) {
-	res.sendFile('./index.html');
+    res.sendFile('./index.html');
 });
+
+
 
 // ROUTES
 router.route('/addSubjectToTracker').get(function(req, res) {
-	var addedSubject;
-	Subject.findOne({
-		trackingId : req.query.subjectTrackingId
-	}, function(err, subject) {
-		addedSubject = subject;
-		console.log(addedSubject);
-	});
-	Tracker.findOne({
-		username : req.query.trackerName
-	}, function(err, tracker) {
-		tracker.subjects.push(addedSubject._id);
-		tracker.save(function(err) {
-			if (err)
-				res.status(500).send('Subject could not be added');
-		})
-		res.status(200).send(tracker);
-	});
+    var addedSubject;
+    Subject.findOne({
+        trackingId: req.query.subjectTrackingId
+    }, function(err, subject) {
+        addedSubject = subject;
+        console.log(addedSubject);
+    });
+    Tracker.findOne({
+        username: req.query.trackerName
+    }, function(err, tracker) {
+        tracker.subjects.push(addedSubject._id);
+        tracker.save(function(err) {
+            if (err)
+                res.status(500).send('Subject could not be added');
+        })
+        res.status(200).send(tracker);
+    });
 });
 
-router.route('/deleteSubjectFromTracker').get(function(req,res){
-	var deleteSub;
-	Subject.findOne({
-		trackingId: req.query.subjectTrackingId
-	}, function(err, subject){
-		deleteSub = subject;
-		console.log(deleteSub);
-	});
-	Tracker.findOne({
-		username: req.query.trackerName
-	}, function(err, tracker){
-		var index = tracker.subjects.indexOf(deleteSub._id);
-		tracker.subjects.splice(index, 1);
-		tracker.save(function(err){
-			if (err)
-				res.status(500).send('Subject could not be deleted');
-		});
-		console.log('Delete subject from tracker success');
-		res.status(200).send(tracker);
-	});
+router.route('/deleteSubjectFromTracker').get(function(req, res) {
+    var deleteSub;
+    Subject.findOne({
+        trackingId: req.query.subjectTrackingId
+    }, function(err, subject) {
+        deleteSub = subject;
+        console.log(deleteSub);
+    });
+    Tracker.findOne({
+        username: req.query.trackerName
+    }, function(err, tracker) {
+        var index = tracker.subjects.indexOf(deleteSub._id);
+        tracker.subjects.splice(index, 1);
+        tracker.save(function(err) {
+            if (err)
+                res.status(500).send('Subject could not be deleted');
+        });
+        console.log('Delete subject from tracker success');
+        res.status(200).send(tracker);
+    });
 });
 
 // router.route('/refreshTracker').get(function(req, res) {
@@ -154,97 +160,107 @@ router.route('/deleteSubjectFromTracker').get(function(req,res){
 // });
 // });
 
+
 router.route('/getTrackerSubjects').get(function(req, res) {
-	var trackerName = req.query.trackerName;
-	var subjectIds = [];
-	Tracker.findOne({
-		username : trackerName
-	}, function(err, tracker) {
-		var subjects = [];
-		subjectIds = tracker.subjects;
-		Subject.find({
-			_id : {
-				$in : subjectIds
-			}
-		}, function(err, subjects) {
-			res.send(subjects);
-		});
-	});
+    var trackerName = req.query.trackerName;
+    var subjectIds = [];
+    Tracker.findOne({
+        username: trackerName
+    }, function(err, tracker) {
+        var subjects = [];
+        if (tracker !== null) {
+            subjectIds = tracker.subjects;
+            Subject.find({
+                _id: {
+                    $in: subjectIds
+                }
+            }, function(err, subjects) {
+                res.send(subjects);
+            });
+        }
+
+    });
 
 });
 
 router.route('/subjectLocation').get(function(req, res) {
-	var lon = req.query.longitude;
-	var lat = req.query.latitude;
-	var subjectName = req.query.subUsername;
-	Subject.findOne({
-		username : subjectName
-	}, function(err, sub) {
-		if (err) {
-			console.log('error find: ' + err)
-			res.status(500).send(err);
-		}
+    var lon = req.query.longitude;
+    var lat = req.query.latitude;
+    var subjectName = req.query.subUsername;
+    Subject.findOne({
+        username: subjectName
+    }, function(err, sub) {
+        if (err) {
+            console.log('error find: ' + err)
+            res.status(500).send(err);
+        }
 
-		request('https://api.mapbox.com/v4/geocode/mapbox.places/' + lon + ',' + lat + '.json?access_token=pk.eyJ1IjoiYW5rYWlqaWUiLCJhIjoiY2lmMmdya3o5MWl4N3Q1bTVvbWl5bTloaiJ9.j0get67vjdd3YJt9nr7o3w', function(error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var json = JSON.parse(body);
-				var location2 = json['features'][0]['place_name'];
-				console.log("ID: " + sub.id);
-				sub.location = location2;
-				sub.save(function(err) {
-					if (err) {
-						console.log('error save:' + err);
-						res.status(500).send(err);
-					}
-				});
-				res.status(200).send(sub);
-			}
-		});
+        request('https://api.mapbox.com/v4/geocode/mapbox.places/' + lon + ',' + lat + '.json?access_token=pk.eyJ1IjoiYW5rYWlqaWUiLCJhIjoiY2lmMmdya3o5MWl4N3Q1bTVvbWl5bTloaiJ9.j0get67vjdd3YJt9nr7o3w', function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var json = JSON.parse(body);
+                var location2 = json['features'][0]['place_name'];
+                console.log("ID: " + sub.id);
+                sub.location = location2;
+                sub.save(function(err) {
+                    if (err) {
+                        console.log('error save:' + err);
+                        res.status(500).send(err);
+                    }
+                });
+                res.status(200).send(sub);
+            }
+        });
 
-	});
+    });
 
 });
 
+
+router.route('/logout').get(function(req, res) {
+    req.logout();
+});
+
+
 router.route('/loginTracker').post(passport.authenticate('tracker'), function(req, res) {
-	console.log('authentication successful: ' + req.user);
-	res.status(200).send(req.user);
+    console.log('authentication successful: ' + req.user);
+    res.status(200).send(req.user);
 });
 
 router.route('/loginSubject').post(passport.authenticate('subject'), function(req, res) {
-	console.log('authentication successful: ' + req.user);
-	res.status(200).send(req.user);
+    console.log('authentication successful: ' + req.user);
+    res.status(200).send(req.user);
 });
 
 router.route('/addNewTracker').post(function(req, res) {
-	Tracker.register(new Tracker({
-		username : req.body.username,
-		subject : []
-	}), req.body.password, function(err, tracker) {
-		if (err) {
-			console.log('Error' + err);
-			res.status(500).send(err);
-		}
-		passport.authenticate('tracker')(req, res, function() {
-			res.status(200).send('Success: ' + tracker);
-		});
-	});
+    Tracker.register(new Tracker({
+        username: req.body.username,
+        subject: []
+    }), req.body.password, function(err, tracker) {
+        if (err) {
+            console.log('Error' + err);
+            res.status(500).send(err);
+        }
+        passport.authenticate('tracker')(req, res, function() {
+            res.status(200).send('Success: ' + tracker);
+        });
+    });
 
 });
 
 router.route('/addNewSubject').post(function(req, res) {
-	Subject.register(new Subject({
-		username : req.body.username,
-		trackingId : Math.floor(Math.random() * 90000 + 10000),
-		location : 'The subject has not yet checked in.'
-	}), req.body.password, function(err, subject) {
-		if (err) {
-			console.log('Error' + err);
-			res.status(500).send(err);
-		}
-		passport.authenticate('subject')(req, res, function() {
-			res.send('Success: ' + subject);
-		});
-	});
+    Subject.register(new Subject({
+        username: req.body.username,
+        trackingId: Math.floor(Math.random() * 90000 + 10000),
+        location: 'The subject has not yet checked in.'
+    }), req.body.password, function(err, subject) {
+        if (err) {
+            console.log('Error' + err);
+            res.status(500).send(err);
+        }
+        passport.authenticate('subject')(req, res, function() {
+            res.send('Success: ' + subject);
+        });
+    });
 
 });
 
